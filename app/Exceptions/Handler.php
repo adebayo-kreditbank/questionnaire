a5234f67a63783ad4559fcd11d4b1ba32a2d067b
Adebayo Olukunle <a.bayo@thekredibank.com>
2024-03-30 20:16:23 +0100

    customize NotFound error message for API

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -28,5 +28,13 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+
+        $this->renderable(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Resource not found on Manual.'
+                ], 404);
+            }
+        });
     }
 }
